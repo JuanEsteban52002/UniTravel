4a252737b6ec74f45af9be57a1ad2888a33e4779
Juan Esteban Granda <juane.grandar@uqvirtual.edu.co>
2022-05-05 00:59:47 -0500

    Correciones

--- negocio/src/test/resources/dataset.sql
+++ negocio/src/test/resources/dataset.sql
@@ -1,6 +1,5 @@
-
-insert into ciudad values (1, "Bogotá");
-insert into ciudad values (2, "Medellín");
+insert into ciudad values (1, "Bogota");
+insert into ciudad values (2, "Medellin");
 insert into ciudad values (3, "Cali");
 insert into ciudad values (4, "Cartagena");
 insert into ciudad values (5, "Barranquilla");
@@ -10,6 +9,8 @@ insert into ciudad values (6, "Santa Marta");
 insert into cliente values ("1234", "juan@gmail,com", "Juan", "5555", 1);
 insert into cliente values ("5255", "pepe@gmail,com", "Pepe", "1234", 2);
 insert into cliente values ("4321", "mario@gmail,com", "Mario", "1234", 3);
+insert into cliente values ("7525", "juanS@gmail,com", "Juan", "5555", 4);
+
 
 
 insert into administrador_hotel values ("5555", "admin1@gmail,com", "Admin 1", "admin1");
@@ -18,6 +19,8 @@ insert into administrador_hotel values ("9999", "admin2@gmail,com", "Admin 2", "
 
 insert into hotel values (1, "Carrera 1", "Hotel 1", 5, "7458241", "5555", 1);
 insert into hotel values (2, "Carrera 2", "Hotel 2", 4, "7489652", "9999", 2);
+insert into hotel values (3, "Carrera 3", "Hotel 3", 4, "7852584", "5555", 1);
+
 
 
 insert into administrador values ("8754", "adminUnitravel1@gmail,com", "AdminUni 1", "adminUni1");
@@ -27,6 +30,7 @@ insert into administrador values ("7852", "adminUnitravel2@gmail,com", "AdminUni
 insert into habitacion values (1, "5", 57000, 1);
 insert into habitacion values (2, "4", 45000, 1);
 insert into habitacion values (3, "3", 35000, 1);
+insert into habitacion values (4, "3", 35000, 1);
 
 
 insert into cama values (1, "Sencilla", 1);
@@ -36,6 +40,14 @@ insert into cama values (3, "Triple", 3 );
 
 insert into caracteristica values (1, "Hay camas");
 insert into caracteristica values (2, "No hay nada");
+insert into caracteristica values (3, "La cama es muy dura");
+insert into caracteristica values (4, "La cama es muy blanda");
+
+insert into caracteristica_hoteles values (1, 1);
+insert into caracteristica_hoteles values (2, 2);
+
+insert into caracteristica_habitaciones values (3, 1);
+insert into caracteristica_habitaciones values (4, 2);
 
 
 insert into aeropuerto values ("A1", "Carrera 1","Aeropuerto Internacional El Dorado", 1);
@@ -76,7 +88,8 @@ insert into telefono values ("3183980410", "Personal", "1234");
 insert into telefono values ("3163601741", "Trabajo", "5255");
 insert into telefono values ("3189541695", "Casa", "4321");
 
+
 insert into comentario values ("1", 5, "Buen servicio", "2022-05-13", "1234", 1);
 insert into comentario values ("2", 4, "Muy buen servicio", "2022-05-15", "5255", 2);
-
-
+insert into comentario values ("3", 3, "Servicio regular", "2022-08-13", "1234", 1);
+insert into comentario values ("4", 2, "Malo servicio", "2022-08-15", "5255", 3);
