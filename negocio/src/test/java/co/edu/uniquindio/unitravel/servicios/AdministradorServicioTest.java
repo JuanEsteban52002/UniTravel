d64f5a5325716b98b8f7d73d81bdaef1271d4cbc
Juan Esteban Granda <juane.grandar@uqvirtual.edu.co>
2022-05-06 13:43:27 -0500

    Test de los servicios
    Se realizaron gran parte de los test del cliente, administrador y administrador hotel aun queda pendiente

--- negocio/src/test/java/co/edu/uniquindio/unitravel/servicios/AdministradorServicioTest.java
+++ negocio/src/test/java/co/edu/uniquindio/unitravel/servicios/AdministradorServicioTest.java
@@ -1,7 +1,9 @@
 package co.edu.uniquindio.unitravel.servicios;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 
 import javax.transaction.Transactional;
 
@@ -11,6 +13,12 @@ public class AdministradorServicioTest {
 
     @Autowired
     private AdministradorServicio administradorServicio;
+
+    @Test
+    @Sql("classpath:dataset.sql")
+    public void validarLoginTest(){
+
+    }
 
 
 }
