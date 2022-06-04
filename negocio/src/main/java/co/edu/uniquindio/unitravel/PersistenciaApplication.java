27c1f7ef577f08d6f380b48565c043f181a3c95c
JUANESTEBAN <juane.grandar@uqvirtual.edu.co>
2022-06-04 16:42:55 -0500

    Exclude data source

--- negocio/src/main/java/co/edu/uniquindio/unitravel/PersistenciaApplication.java
+++ negocio/src/main/java/co/edu/uniquindio/unitravel/PersistenciaApplication.java
@@ -7,11 +7,12 @@ import org.jasypt.util.password.StrongPasswordEncryptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 import java.util.List;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class PersistenciaApplication {
 
     public static void main(String[] args) {
