6ee072f864ed4a2bd85ad4cef767e93123b96c88
JUANESTEBAN <juane.grandar@uqvirtual.edu.co>
2022-06-05 01:38:49 -0500

    Prueba

--- web/src/main/java/co/edu/uniquindio/unitravel/config/DatosIniciales.java
+++ web/src/main/java/co/edu/uniquindio/unitravel/config/DatosIniciales.java
@@ -28,13 +28,7 @@ public class DatosIniciales implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         if (unitravelUtilServicio.listarCiudades().isEmpty()) {
-            administradorServicio.crearCiudad(new Ciudad("Armenia", "https://parlamentoandino.org/images/P-Fundacin-de-Armenia.jpg%22"));
-            administradorServicio.crearCiudad(new Ciudad("Bogotá", "https://upload.wikimedia.org/wikipedia/commons/2/24/Bogot%C3%A1_Colpatria_Night.jpg%22"));
-            administradorServicio.crearCiudad(new Ciudad("Santa Marta", "https://cloudfront-us-east-1.images.arcpublishing.com/semana/W2USLWLWJ5FF3IJKDEHFYZSC5I.jpg"));
-            administradorServicio.crearCiudad(new Ciudad("Pereira", "https://cdn.colombia.com/sdi/2022/01/13/turismo-pereira-top-25-987237.jpg%22"));
-            administradorServicio.crearCiudad(new Ciudad("Cartagena", "https://cdn.colombia.com/images/v2/turismo/sitios-turisticos/cartagena/ciudad-cartagena-800.jpg%22"));
-            administradorServicio.crearCiudad(new Ciudad("Barrancabermeja", "https://www.terminaldetransporte.gov.co/sites/default/files/2021-06/barrancabermeja.jpg%22"));
-            administradorServicio.crearCiudad(new Ciudad("Manizales", "https://i0.wp.com/www.bcnoticias.com.co/wp-content/uploads/2019/05/UJFRDKZTCK_20190520172114.jpeg?resize=600%2C399%22"));
+            
         }
     }
 }
