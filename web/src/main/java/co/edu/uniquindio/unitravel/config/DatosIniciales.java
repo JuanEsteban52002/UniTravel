edcd1c8013dd55317818ca5fed400afa7f3241e1
JUANESTEBAN <juane.grandar@uqvirtual.edu.co>
2022-06-08 16:48:47 -0500

    Inicializar 3

--- web/src/main/java/co/edu/uniquindio/unitravel/config/DatosIniciales.java
+++ web/src/main/java/co/edu/uniquindio/unitravel/config/DatosIniciales.java
@@ -27,13 +27,11 @@ public class DatosIniciales implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-            try{
+
                 administradorServicio.crearCiudad(new Ciudad("Medellin", "https://traveler.marriott.com/es/wp-content/uploads/sites/2/2021/01/GI-529527806-Medellin-1920x1080.png"));
                 administradorServicio.crearCiudad(new Ciudad("Armenia", "https://www.desktodirtbag.com/wp-content/uploads/2017/05/armenia-colombia-11.jpg"));
                 administradorServicio.crearCiudad(new Ciudad("Cartagena", "https://i2.wp.com/ail.ens.org.co/wp-content/uploads/sites/3/2020/03/Cartagena.jpg?fit=1540%2C1024&ssl=1"));
                 administradorServicio.crearCiudad(new Ciudad("Santa Marta", "https://traveler.marriott.com/es/wp-content/uploads/sites/2/2021/06/GI-1078056138-Santa-Marta.jpg"));
-            }catch (Exception e){
-                e.printStackTrace();
-            }
+            
     }
 }
