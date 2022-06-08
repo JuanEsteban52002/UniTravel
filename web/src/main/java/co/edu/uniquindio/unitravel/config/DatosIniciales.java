f309b1e9c85fce11972f0946d091e2aa3e968090
JUANESTEBAN <juane.grandar@uqvirtual.edu.co>
2022-06-08 17:26:50 -0500

    Inicializar 526

--- web/src/main/java/co/edu/uniquindio/unitravel/config/DatosIniciales.java
+++ web/src/main/java/co/edu/uniquindio/unitravel/config/DatosIniciales.java
@@ -27,12 +27,12 @@ public class DatosIniciales implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        
-            administradorServicio.crearCiudad(new Ciudad("Medellin", "https://traveler.marriott.com/es/wp-content/uploads/sites/2/2021/01/GI-529527806-Medellin-1920x1080.png"));
-            administradorServicio.crearCiudad(new Ciudad("Armenia", "https://www.desktodirtbag.com/wp-content/uploads/2017/05/armenia-colombia-11.jpg"));
-            administradorServicio.crearCiudad(new Ciudad("Cartagena", "https://i2.wp.com/ail.ens.org.co/wp-content/uploads/sites/3/2020/03/Cartagena.jpg?fit=1540%2C1024&ssl=1"));
-            administradorServicio.crearCiudad(new Ciudad("Santa Marta", "https://traveler.marriott.com/es/wp-content/uploads/sites/2/2021/06/GI-1078056138-Santa-Marta.jpg"));
-
+        if(unitravelUtilServicio.listarCiudades().isEmpty()){
+            administradorServicio.crearCiudad(new Ciudad("Medellin"));
+            administradorServicio.crearCiudad(new Ciudad("Armenia"));
+            administradorServicio.crearCiudad(new Ciudad("Cartagena"));
+            administradorServicio.crearCiudad(new Ciudad("Santa Marta"));
+        }
 
     }
 }
